8da13075a9cc40e36a1cdb61808389b4ff83ad7a
Sahil Ahmed <sahil.98@hotmail.com>
2023-02-18 19:27:47 +0000

    set variable id and add try and catch statement

--- mens.php
+++ mens.php
@@ -1,6 +1,6 @@
 <?php include 'template-header.php';
 
-// require 'includes/db.con.php';
+require 'includes/db.con.php';
 
 // $query = 'SELECT * FROM products';
 
@@ -11,6 +11,14 @@
 // mysqli_free_result($result);
 
 // mysqli_close($conn);
+
+try {
+  $id = $_GET['id'];
+
+  
+} catch{
+
+}
 ?>
 <html>
   <head lang="en-GB" dir="ltr">
@@ -28,57 +36,57 @@
   <main id="maincontent">
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/brown_suede_shoes.jpg" alt="brown suede shoes"></a>
+      <img src="img/men_shoes/brown_suede_shoes.jpg" alt="brown suede shoes"></a>
       <blockquote><b>&pound;40.00</b></blockquote>
       <figcaption>Brown Suede Shoes</figcaption>
     </figure>
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/navy_derby_shoes.jpg" alt="navy derby shoes"></a>
+      <img src="img/men_shoes/navy_derby_shoes.jpg" alt="navy derby shoes"></a>
       <blockquote><b>&pound;40.00</b></blockquote>
       <figcaption>Navy Suede Shoes</figcaption>
     </figure>
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/black_suede_shoes.jpg" alt="black suede shoes"></a>
+      <img src="img/men_shoes/black_suede_shoes.jpg" alt="black suede shoes"></a>
       <blockquote><b>&pound;50.00</b></blockquote>
       <figcaption>David Beckham Black Suede Shoes</figcaption>
     </figure>
 
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/black_leather_shoes.jpg" alt="black leather shoes"></a>
+      <img src="img/men_shoes/black_leather_shoes.jpg" alt="black leather shoes"></a>
       <blockquote><b>&pound;30.00</b></blockquote>
       <figcaption>Black Leather derby shoes</figcaption>
     </figure>
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/tan_leather_shoes.jpg" alt="tan leather shoes"></a>
+      <img src="img/men_shoes/tan_leather_shoes.jpg" alt="tan leather shoes"></a>
       <blockquote><b>&pound;25.00</b></blockquote>
       <figcaption>Tan leather shoes</figcaption>
     </figure>
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/black_stich_leather_shoes.jpg" alt="black stich look shoes"></a>
+      <img src="img/men_shoes/black_stich_leather_shoes.jpg" alt="black stich look shoes"></a>
       <blockquote><b>&pound;32.00</b></blockquote>
       <figcaption>Black Stitch Look Shoes</figcaption>
     </figure>
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/burgundy_shoes.jpg" alt="burgundy shoes"></a>
+      <img src="img/men_shoes/burgundy_shoes.jpg" alt="burgundy shoes"></a>
       <blockquote><b>£45.00</b></blockquote>
       <figcaption>Burgundy Shoes</figcaption>
     </figure>
 
 
     <figure>
-      <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>"><img src="img/men_shoes/black_strap_shoes.jpg" alt="black strap leather shoes"></a>
+      <img src="img/men_shoes/black_strap_shoes.jpg" alt="black strap leather shoes"></a>
       <blockquote><b>£50.00</b></blockquote>
       <figcaption>Black strap leather shoes</figcaption>
     </figure>
 
     <figure>
-    <a href="shoes.php?shoes_ID=<?php echo $row['ID'] ?>">  <img src="img/men_shoes/brown_stitch_shoes.jpg" alt="brown stich shoes"></a>
+      <img src="img/men_shoes/brown_stitch_shoes.jpg" alt="brown stich shoes"></a>
       <blockquote><b>£50.00</b></blockquote>
       <figcaption>Brown stitch shoes</figcaption>
     </figure>
